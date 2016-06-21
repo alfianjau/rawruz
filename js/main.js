699e3798016cf6046dce6dfad7b65cf24c968db5
Muhammad Alfian Jauhari <alfianjauhari@gmail.com>
2016-06-21 20:54:30 +0700

    add padding and css3 patterns

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 jQuery(document).ready(function() {
     jQuery('#fullpage').fullpage({
-    	paddingTop: '15px',
-        paddingBottom: '15px',
+    	paddingTop: '3em',
+        paddingBottom: '10px',
         //Navigation
         anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage'],
-        sectionsColor: ['#7F6542','#FFCA84','#FFEBD1', '#7F6645'],
+        sectionsColor: ['#272727','#FFCA84','#FFEBD1', '#7F6645'],
         navigation: true,
         navigationPosition: 'right',
         navigationTooltips: ['First page', 'Second page', 'Third page', 'last page']
