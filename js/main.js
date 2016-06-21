b63313c65210a9f427a787fab089e96aa14182f7
Muhammad Alfian Jauhari <alfianjauhari@gmail.com>
2016-06-21 20:38:07 +0700

    modified layout adjustment

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 jQuery(document).ready(function() {
     jQuery('#fullpage').fullpage({
+    	paddingTop: '15px',
+        paddingBottom: '15px',
         //Navigation
         anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage'],
         sectionsColor: ['#7F6542','#FFCA84','#FFEBD1', '#7F6645'],
         navigation: true,
         navigationPosition: 'right',
         navigationTooltips: ['First page', 'Second page', 'Third page', 'last page']
+        
     });
 });
